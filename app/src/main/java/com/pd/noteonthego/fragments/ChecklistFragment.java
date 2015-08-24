e6b2c642ecc365ec0e0818d30778372863d00d34
PrasanjitDey <pdey@iaai.com>
2015-08-24 19:15:28 +0530

    Gson added in gradle. Checklist fragment added.

---
+++ app/src/main/java/com/pd/noteonthego/fragments/ChecklistFragment.java
@@ -0,0 +1,81 @@
+package com.pd.noteonthego.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pd.noteonthego.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ChecklistFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class ChecklistFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ChecklistFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_checklist, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction();
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction();
+    }
+
+}
--- app/src/main/java/com/pd/noteonthego/fragments/NotesFragment.java
+++ app/src/main/java/com/pd/noteonthego/fragments/NotesFragment.java
@@ -8,7 +8,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -77,13 +76,6 @@ public class NotesFragment extends Fragment {
         mNoteExtras.setText(dateTime);
 
         mNoteContainer = (RelativeLayout) getActivity().findViewById(R.id.note_container);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            // mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
